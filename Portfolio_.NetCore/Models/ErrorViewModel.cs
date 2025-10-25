757f04b03355f9b1b7d23bfe190589299e1630d0
Resul Karademir <resulkarademir23@gmail.com>
2025-10-25 23:55:45 +0300

    Add project files.

---
+++ Portfolio_.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfolio_.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
